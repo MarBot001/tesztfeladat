de2d090cef660421db79b9a0ca7519b28e718363
Marbot001 <droidhun01@gmail.com>
2025-06-14 10:50:16 +0200

    Userek belépési adatainak kiírása PHP-be

---
+++ users.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    [
+        'id'       => 1,
+        'username' => 'admin',
+        // jelszó: admin123
+        'password' => '$2y$12$P9HCgJkFg4GibkiGcGjOZ.iJWnaIfBvKOA9dTG0Uyz7Kc6Zip0xTm',
+        'role'     => 'admin',
+        'auth_key' => 'adminkey',
+    ],
+    [
+        'id'       => 2,
+        'username' => 'student',
+        // jelszó: student123
+        'password' => '$2y$12$TE2GMCT835b/xYuW1l3QYeb0orVZTGfBQoAmc0cNDSj0zflxu8eDG',
+        'role'     => 'student',
+        'auth_key' => 'studentkey',
+    ],
+];
